5cff638ea84ab8bef64d55fc689013081f86137a
Crude Creations <132678134+CrudeCreations@users.noreply.github.com>
2024-12-20 20:34:01 +0000

    build: fix date format

--- release.js
+++ release.js
@@ -2,6 +2,7 @@ import fs from 'fs';
 import path from 'path';
 import yaml from 'js-yaml';
 import crypto from 'crypto';
+const { format } = require('date-fns');  
 
 const args = process.argv.slice(2);
 const [version, outDir, zipName] = args;
@@ -10,12 +11,13 @@ const pluginData = yaml.load(fs.readFileSync('set-image-pornpics.yml', 'utf8'));
 
 const zipData = fs.readFileSync(zipName);
 const sha256Hash = crypto.createHash('sha256').update(zipData).digest('hex');
+const formattedDate = format(new Date(), 'yyyy-MM-dd HH:mm:ss');
 
 const manifestData = {
   id: pluginData.id,
   name: pluginData.name,
   version,
-  date: new Date().toISOString(),
+  date: formattedDate,
   path: `https://github.com/${process.env.GITHUB_REPOSITORY}/releases/download/v${version}/${zipName}`,
   sha256: sha256Hash
 };
